ce5c2108e9d264969d896caa8b160e3032f2475a
AleksandarSapic <shapke10@gmail.com>
2024-10-02 19:10:53 +0200

    Configurations, styles and static content

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {ProfilePageComponent} from "./pages/profile-page/profile-page.component";
+import {ExamsPageComponent} from "./pages/exams-page/exams-page.component";
+import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
+import {AdminProfilePageComponent} from "./pages/admin-profile-page/admin-profile-page.component";
+import {AdminDashboardPageComponent} from "./pages/admin-dashboard-page/admin-dashboard-page.component";
+import {ExamEventDetailPageComponent} from "./pages/exam-event-detail-page/exam-event-detail-page.component";
+import {UserDetailPageComponent} from "./pages/user-detail-page/user-detail-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'events', component: ExamsPageComponent},
+  {path: 'exam-events/:eventId', component: ExamEventDetailPageComponent},
+  {path: 'users/:userId', component: UserDetailPageComponent},
+  {path: 'profile', component: ProfilePageComponent},
+  {path: 'admin/profile', component: AdminProfilePageComponent},
+  {path: 'admin/dashboard', component: AdminDashboardPageComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
