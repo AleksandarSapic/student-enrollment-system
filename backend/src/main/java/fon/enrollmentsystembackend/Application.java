0697029cc4d03dac6d5b6f7fa3474107aa702ffd
AleksandarSapic <shapke10@gmail.com>
2024-10-02 19:04:15 +0200

    Implemented libraries and properties file

--- backend/src/main/java/fon/enrollmentsystembackend/Application.java
+++ backend/src/main/java/fon/enrollmentsystembackend/Application.java
@@ -1,13 +1,22 @@
 package fon.enrollmentsystembackend;
 
+import fon.enrollmentsystembackend.controller.ApplicationFormController;
+import fon.enrollmentsystembackend.controller.ExamEventController;
+import fon.enrollmentsystembackend.controller.UserController;
+import fon.enrollmentsystembackend.model.Admin;
+import fon.enrollmentsystembackend.repository.AdminRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
